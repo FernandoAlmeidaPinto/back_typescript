31215d16abb272c85bccaf318f6dbb28ebbe4a9a
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2021-08-07 18:50:40 -0300

    nova rota deleteuser

--- start/routes.ts
+++ start/routes.ts
@@ -6,6 +6,7 @@ Route.get('/logout', 'AuthController.logout').middleware('auth')
 Route.get('/me', 'AuthController.me').middleware('auth')
 Route.put('/patchme', 'AuthController.patchme').middleware('auth')
 Route.patch('/patchadmin', 'AuthController.patchAdmin').middleware('auth')
+Route.get('/deleteuser/:id', 'AuthController.delete').middleware('auth')
 
 Route.post('/esqueci-minha-senha', 'ForgotsController.forgot')
 Route.patch('/reset', 'ForgotsController.reset')
