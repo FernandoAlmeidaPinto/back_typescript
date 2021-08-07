b541ea54ee6593b4aa0e3798e7ff7a3ccc4bb91a
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2021-08-06 23:17:57 -0300

    error de commit

--- test/simulate.spec.ts
+++ test/simulate.spec.ts
@@ -1,12 +1,27 @@
 import test from 'japa'
+import supertest from 'supertest'
+import { baseURL, send} from './config'
 
-test.group('Simulados', () => {
-  /* group.before(async () => {
-    const login = await supertest(BASE_URL).post('/login').expect(200).send({
+test.group('Simulados', (group) => {
+  group.before(async (assert) => {
+
+    const sendAdmin = send;
+
+    sendAdmin.email = "admin@gmail.com"
+    sendAdmin.professor = 1
+    sendAdmin.password_confirmation = sendAdmin.password
+
+    const cadastro = await supertest(baseURL)
+                            .post('/cadastro')
+                            .expect(200)
+                            .send(sendAdmin)
+    assert.equal(1,1)
+
+    const login = await supertest(baseURL).post('/login').expect(200).send({
         email: send.email,
         password: send.password,
       })
-  }) */
+  })
 
   test('2+2', async (assert) => {
     assert.equal(2 + 2, 4)
