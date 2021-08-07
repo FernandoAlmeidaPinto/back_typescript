1a4cac22961cb07ea8d80d81e21da1cff969c253
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2021-08-07 18:33:21 -0300

    variáveis para test

---
+++ test/config.ts
@@ -0,0 +1,28 @@
+export const baseURL = 'http://localhost:8080'
+
+export const send = {
+    email: 'test@gmail.com',
+    password: '123456',
+    password_confirmation: '123456',
+    nome: 'Test',
+    sobrenome: 'Adonis',
+    telefone: '(11)9xxxx-xxxx',
+    genero: 'Masculino',
+    nascimento: '1989-06-26',
+    estado: 'SP',
+    cidade: 'São Paulo',
+    professor: 0,
+}
+
+export const fields = [
+    'email',
+    'password',
+    'nome',
+    'sobrenome',
+    'telefone',
+    'genero',
+    'nascimento',
+    'estado',
+    'cidade',
+    'professor',
+]
