0c1c63c1ed3f49ffd6365bce7a0d8d4c6347e716
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2021-08-29 19:42:10 -0300

    refatorando teste

--- test/simulate.spec.ts
+++ test/simulate.spec.ts
@@ -3,28 +3,15 @@ import supertest from 'supertest'
 import { baseURL, send} from './config'
 
 test.group('Simulados', (group) => {
-  group.before(async (assert) => {
+  group.before(async () => {
 
     const sendAdmin = send;
 
-    sendAdmin.email = "admin@gmail.com"
+    sendAdmin.email = "simulado@gmail.com"
     sendAdmin.professor = 1
     sendAdmin.password_confirmation = sendAdmin.password
 
-    const cadastro = await supertest(baseURL)
-                            .post('/cadastro')
-                            .expect(200)
-                            .send(sendAdmin)
-    assert.equal(1,1)
-
-    const login = await supertest(baseURL).post('/login').expect(200).send({
-        email: send.email,
-        password: send.password,
-      })
+    await supertest(baseURL).post('/cadastro').expect(200).send(sendAdmin)
   })
 
-  test('2+2', async (assert) => {
-    assert.equal(2 + 2, 4)
-  }) 
 })
-
