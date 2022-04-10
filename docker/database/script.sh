377dada3d32bd33a1757b834aef9106f2fd91854
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2022-04-10 16:25:19 -0300

    fix: atualizaçao do droplet

--- docker/database/script.sh
+++ docker/database/script.sh
@@ -2,6 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$PG_USER" <<-EOSQL
-    CREATE DATABASE adonis;
-    GRANT ALL PRIVILEGES ON DATABASE adonis TO vcnafacul;
+    CREATE DATABASE vcnafacul;
+    GRANT ALL PRIVILEGES ON DATABASE vcnafacul TO vcnafacul;
 EOSQL
--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+yarn
 yarn build
 cp .env ./build
 cd ./build
 yarn install
 cd ..
 docker-compose up --build -d
+yarn migration
